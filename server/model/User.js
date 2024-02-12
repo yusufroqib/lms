55a7f47553289a59c7251296c13226a495de08d6
yusufroqib <yusufroqib@gmail.com>
2024-02-12 10:37:14 +0100

    update

--- server/model/User.js
+++ server/model/User.js
@@ -1,24 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    roles: {
-        Student: {
-            type: Number,
-            default: "Student"
-        },
-        Editor: String,
-        Admin: String
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    refreshToken: String
-});
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		avatar: String,
+        bio: String,
+		followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+		following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+		roles: {
+			Student: {
+				type: Number,
+				default: "Student",
+			},
+			Editor: String,
+			Admin: String,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		enrolledCourses: [{ type: Schema.Types.ObjectId, ref: "Course"  }],
+		createdCourses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+		refreshToken: String,
+	},
+	{
+		timestamps: true,
+	}
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
