2e03c3f2b838bdb94d80f57d25b506f28420e04d
yusufroqib <yusufroqib@gmail.com>
2024-02-24 18:39:44 +0100

    update

--- client/src/pages/courses/CoursesIndex.jsx
+++ client/src/pages/courses/CoursesIndex.jsx
@@ -1,7 +1,7 @@
 // import { Button } from "@material-tailwind/react";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const CoursesIndex = () => {
     const navigate = useNavigate()
@@ -14,16 +14,16 @@ const CoursesIndex = () => {
 			</div>
 			<div className=" h-full flex-col flex w-full justify-center items-center">
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-12 ">
-					<div className=" bg-slate-800 transition-all ease-in md:hover:bg-slate-700 md:hover:scale-110 duration-300 cursor-pointer  h-80 w-80 rounded-xl flex justify-center items-center">
+					<Link to='/dashboard' className=" bg-slate-800 transition-all ease-in md:hover:bg-slate-700 md:hover:scale-110 duration-300 cursor-pointer  h-80 w-80 rounded-xl flex justify-center items-center">
 						<h2 className="text-whiten text-3xl text-center">
 							Enrolled Courses
 						</h2>
-					</div>
-					<div className=" bg-slate-800 shadow-card-2 transition-all ease-in md:hover:bg-slate-700 md:hover:scale-110 duration-300 cursor-pointer  h-80 w-80 rounded-xl flex justify-center items-center">
+					</Link>
+					<Link to='/dashboard' className=" bg-slate-800 shadow-card-2 transition-all ease-in md:hover:bg-slate-700 md:hover:scale-110 duration-300 cursor-pointer  h-80 w-80 rounded-xl flex justify-center items-center">
 						<h2 className="text-whiten text-3xl text-center">
 							Browse Courses Catalog
 						</h2>
-					</div>
+					</Link>
 				</div>
 				<Button onClick={() => navigate('/tutors/my-courses')} className="mt-12 py-3 text-2xl bg-primary transition-all duration-400 hover:bg-slate-800 rounded-lg ">
 					Tutor's Page
