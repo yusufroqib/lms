7e1ec23bbc14c41d246799fa8a14b0ded4844241
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-07-18 21:04:06 +0100

    fix google callback uri

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,14 +1,18 @@
 const passport = require('passport');
 const User = require('../models/UserModel');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const backendUrl = process.env.BACKEND_URL;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const callbackUrl = process.env.CALLBACK_URL;
+// console.log(backendUrl)
 
 
 // const initializePassport = (passport) => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${backendUrl}/auth/google/callback`, // Adjust accordingly
+    callbackURL: callbackUrl, // Adjust accordingly
     scope: ['profile', 'email'],
   },
   async (accessToken, refreshToken, profile, done) => {
