972812136ecc206396119fe4d599ca271d50e778
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-07-18 18:30:13 +0100

    update variables

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,13 +2,14 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+
 const allowedOrigins = [
+	process.env.CLIENT_URL,
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"http://localhost:3000",
 	"http://localhost:3002",
 	"http://localhost:3001",
-	process.env.CLIENT_URL,
 ];
 
 module.exports = allowedOrigins;
