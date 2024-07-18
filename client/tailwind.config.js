05bbcebb6e7b043519b65110b3fd66ef94d93d3e
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-07-18 14:25:53 +0100

    update to config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
+
 export default withMT({
 	// darkMode: "class",
 	darkMode: "false",
