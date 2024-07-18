9311e4e2cefea406fec25c84da31a1d662894db3
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-07-18 21:18:13 +0100

    fix scroll reset on reload

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 import "./init";
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
@@ -13,21 +13,30 @@ import { ConfettiProvider } from "./components/providers/ConfettiProvider";
 import { registerLicense } from "@syncfusion/ej2-base";
 import "react-circular-progressbar/dist/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
+import { resetScroll } from "./lib/scrollReset";
 
 registerLicense(import.meta.env.VITE_EJ2_LICENSE_KEY);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<Provider store={store}>
-				<ThemeProvider>
-					<ConfettiProvider />
-					<Toaster />
-					<BrowserRouter>
-						<Routes>
-							<Route path="/*" element={<App />} />
-						</Routes>
-					</BrowserRouter>
-				</ThemeProvider>
-		</Provider>
-	</React.StrictMode>
-);
+function Root() {
+  useEffect(() => {
+    resetScroll();
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <ThemeProvider>
+          <ConfettiProvider />
+          <Toaster />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
